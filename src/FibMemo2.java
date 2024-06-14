63693c08a6e4b2a14c40b95d9eb4480bf6225404
SLAVON <nslava@gmail.com>
2024-06-14 16:13:36 +0300

    Algs Alishev 1-10

---
+++ src/FibMemo2.java
@@ -0,0 +1,28 @@
+import java.time.LocalTime;
+import java.util.Arrays;
+
+public class FibMemo2 {
+    //  MemoFibonacci algoritm
+
+    public static void main(String[] args) {
+        int fib = 100;
+        long[] mem = new long[fib + 1];
+        Arrays.fill(mem, -1);
+           System.out.println(LocalTime.now());
+        System.out.println("FibNaive: " + fibNaive(fib, mem));
+
+
+           System.out.println(LocalTime.now());
+    }
+
+    //O(n)
+    public static long fibNaive(int n, long[] mem) {
+        if (mem[n] != -1)  // проверка массива на уже вычисленный элемент экономит много времени!!!!! (~7 секунд на старом процессоре)
+            return mem[n];
+        if (n <= 1)          //   f(N) = { n=0 - 0, n=1 -1, n>1 - Fn-1+Fn-2 }
+          return n;
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem); //recursive when non-effective for fib=45+ 10+ seconds
+        mem[n] = result;
+        return result;
+    }
+}
---
+++ src/FibNaive1.java
@@ -0,0 +1,33 @@
+public class FibNaive1 {
+    // Fibonacci algiritm
+
+    public static void main(String[] args) {
+        int fib=10;
+                System.out.println("FibNaive: "+ fibNaive(fib)+"\nFibEff: " + fibEff(fib) );
+    }
+    // O(2^n)
+    public static long fibNaive(int n) {
+        if(n<=1)          //   f(N) = { n=0 - 0, n=1 -1, n>1 - Fn-1+Fn-2 }
+            return n;
+
+        return fibNaive(n-1)+fibNaive(n-2); //recursive non-effective for 45+ 10+ seconds
+
+    }
+    // O(n)
+    public static long fibEff(int n) {
+
+        long[] arr =new long[n+1];  // O(n)
+        if(n<=1)          //   f(N) = { n=0 - 0, n=1 -1, n>1 - Fn-1+Fn-2 }
+           return n;
+
+        arr[0]=0; // O(1)
+        arr[1]=1; // O(1)
+        // O(n+n) = O(2n)= O(n)
+        for(int i=2;i<=n; i++)
+            arr[i]=arr[i-1]+arr[i-2];
+        return arr[n]; // O(1)
+        }
+// O(n) = O(n)+O(1)+O(1)+O(n)+O(1) = O(2n)+O(3) = O(2n+3) = O(n)
+
+
+    }
