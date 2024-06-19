0722d7cbef48d6b74a5cf9989d0f3d554a79295b
SLAVON <nslava@gmail.com>
2024-06-19 07:46:50 +0300

    Algs Alishev 1-10

---
+++ src/GreedyAlg2.java
@@ -0,0 +1,13 @@
+public class GreedyAlg2 {
+    public static void main(String[] args) {
+        int [] stations={0,200,375,550,750,950};
+
+        System.out.println(minstops(stations, 400));
+    }
+
+    public static int  minstops(int [] stations, int kmetres) {
+        return 1;
+    }
+
+
+}
