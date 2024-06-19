2587ed1995e35c10975e077dbcb925f4ce78e60f
SLAVON <nslava@gmail.com>
2024-06-19 18:27:07 +0300

    Algs Alishev 1-10

--- src/GreedyAlg2.java
+++ src/GreedyAlg2.java
@@ -4,9 +4,33 @@ public class GreedyAlg2 {
 
         System.out.println(minstops(stations, 400));
     }
-
+// -
     public static int  minstops(int [] stations, int kmetres) {
-        return 1;
+        int result=0;     //optimal stops
+        int currentStop=0; //current
+
+    while (currentStop<stations.length-1) {
+        int nextStop=currentStop;
+            while (nextStop<stations.length -1 &&
+                    stations[nextStop+1] - stations[currentStop] <=kmetres)
+                nextStop++;
+
+                if(currentStop==nextStop) // elsi dohrena
+                    return  -1;
+                if(nextStop<stations.length - 1 )
+                    result++;
+
+                currentStop=nextStop;
+
+
+
+        }
+
+
+
+
+
+        return result;
     }
 
 
